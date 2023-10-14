722b5b3105cbef0cdfe01b15bb6a3ec80c390799
anastasiia341 <135730140+anastasiia341@users.noreply.github.com>
2023-10-14 21:48:13 +0200

    edit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,7 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
 const galleryList = document.querySelector(".gallery");
 const galleryMarkup = createGalleryMarkup(galleryItems);
 galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
